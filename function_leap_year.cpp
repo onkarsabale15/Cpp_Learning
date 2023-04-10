a19bc242599c360d012736c63d8bb4c09ab78047
onkarsabale15 <onkarsabale15@gmail.com>
2023-04-10 16:18:57 +0530

    added functions

---
+++ function_leap_year.cpp
@@ -0,0 +1,28 @@
+ #include <iostream>
+ using namespace std;
+
+//return_type function_name(input_type input_value){
+//  logic_Of__Function
+//}
+
+
+void isLeapYear(int year){
+    if(year%100 == 0){
+        if(year%400 == 0){
+            cout<< year<< "is Leap year!!";
+        }else{
+            cout<<year<< " is Not Leap year!!";
+        };
+    }else if(year%4 == 0){
+        cout<< year<< "is Leap year!!";
+    }else{
+        cout<<year<< " is Not Leap year!!";
+    };
+};
+
+
+
+int main(){
+    isLeapYear(1900);
+    return 0;
+ }
